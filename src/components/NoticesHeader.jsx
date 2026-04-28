05b34cf4c0f0492eee8ebac27444e89f666c65e6
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2026-04-28 12:27:15 +0530

    Made changes in the notice section

--- src/components/NoticesHeader.jsx
+++ src/components/NoticesHeader.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const NoticesHeader = () => {
   return (
     <div>
-      <div className="bg-blue-500 text-white py-16 text-center">
-        <h1 className="text-3xl md:text-4xl font-semibold">Notices</h1>
-        <p className="mt-2 text-lg">
+      <div className="bg-blue-500 text-white py-4 lg:py-16 text-center">
+        <h1 className="text-2xl md:text-4xl font-semibold">Notices</h1>
+        <p className="mt-2 lg:text-lg">
           Stay updated with the latest announcements and notices
         </p>
         <p className="mt-2 text-sm">
--- src/components/NoticesSection.jsx
+++ src/components/NoticesSection.jsx
@@ -38,42 +38,42 @@ const NoticesSection = () => {
   const college_notices = [
     {
       title: "Bonafide Certificate Issue Notice",
-      date: "09 Apr 2026",
+      date: "Date: 09-04-26",
       path: "/college_notices/bonafide_issue_notice.pdf",
     },
     {
       title: "Holiday Notice (Ram Navami)",
-      date: "26 Mar 2026",
+      date: "Date: 26-03-26",
       path: "/college_notices/ram_navami_notice.pdf",
     },
     {
       title: "Holiday Notice (Eid al-Fitr)",
-      date: "20 Mar 2026",
+      date: "Date: 20-03-26",
       path: "/college_notices/eid_al_fitr.pdf",
     },
     {
       title: "Examination Form Filling Notice",
-      date: "19 Mar 2026",
+      date: "Date: 19-03-26",
       path: "/college_notices/examination_form_fillup_notice.pdf",
     },
     {
       title: "Holiday Notice (Holi)",
-      date: "28 Feb 2026",
+      date: "Date: 28-02-26",
       path: "/college_notices/holi_notice.pdf",
     },
     {
       title: "Internal Examination Schedule Notice",
-      date: "15 Jan 2026",
+      date: "Date: 15-01-26",
       path: "/college_notices/examination_schedule.pdf",
     },
     {
       title: "Holiday Notice (Makar Sankranti)",
-      date: "13 Jan 2026",
+      date: "Date: 13-01-26",
       path: "/college_notices/makar_sankranti.pdf",
     },
     {
       title: "Holiday Notice (Christmas & Winter Vacation)",
-      date: "24 Dec 2025",
+      date: "Date: 24-12-25",
       path: "/college_notices/christmas_winter_vacation.pdf",
     },
   ];
@@ -81,27 +81,27 @@ const NoticesSection = () => {
   const university_notices = [
     {
       title: "Revised Exam Schedule Notice | BCA (2025-28) | Sem: 01",
-      date: "16 Apr 2026",
+      date: "Date: 16-04-26",
       path: "/university_notices/vbu_revised_examination_centre.pdf",
     },
     {
       title: "Examination Schedule Notice | BCA (2025-28) | Sem: 01",
-      date: "09 Apr 2026",
+      date: "Date: 09-04-26",
       path: "/university_notices/vbu_external_examination_schedule.pdf",
     },
     {
       title: "Examination Centre Notice | BCA (2025-28) | Sem: 01",
-      date: "09 Apr 2026",
+      date: "Date: 09-04-26",
       path: "/university_notices/vbu_examination_centre_notice.pdf",
     },
     {
       title: "VBU Revised Examination form fillup Notice",
-      date: "30 Mar 2026",
+      date: "Date: 30-03-26",
       path: "/university_notices/vbu_revised_examination_form_notice.pdf",
     },
     {
       title: "VBU Examination form fillup Notice",
-      date: "12 Mar 2026",
+      date: "Date: 12-03-26",
       path: "/university_notices/vbu_examination_form_notice.pdf",
     },
   ];
@@ -129,12 +129,19 @@ const NoticesSection = () => {
               {loopedCollege.map((item, index) => (
                 <div
                   key={index}
-                  className="flex justify-between items-center px-2 lg:px-4 py-3 border-b border-gray-300 hover:bg-gray-50"
+                  className="flex justify-between gap-1 items-center px-2 lg:px-4 py-3 border-b border-gray-300 hover:bg-gray-50"
                 >
-                  <span className="font-medium text-xs text-gray-900">
-                    📢 {item.title}
-                  </span>
+                  <p>
+                    <span className="font-medium lg:text-sm text-xs text-gray-900">
+                      📢 {item.title}
+                    </span>
 
+                    <span className="hidden sm:inline"> {" | "} </span>
+
+                    <span className="block sm:inline text-xs lg:text-sm text-gray-500">
+                      {item.date}
+                    </span>
+                  </p>
                   <div className="flex items-center gap-3">
                     <a
                       href={item.path}
@@ -144,9 +151,6 @@ const NoticesSection = () => {
                       <FaFilePdf className="text-red-600" />
                       Download
                     </a>
-                    <span className="text-xs lg:text-sm text-gray-500">
-                      {item.date}
-                    </span>
                   </div>
                 </div>
               ))}
@@ -171,11 +175,19 @@ const NoticesSection = () => {
               {loopedUniversity.map((item, index) => (
                 <div
                   key={index}
-                  className="flex justify-between items-center px-2 lg:px-4 py-3 border-b border-gray-300 hover:bg-gray-50"
+                  className="flex justify-between gap-1 items-center px-2 lg:px-4 py-3 border-b border-gray-300 hover:bg-gray-50"
                 >
-                  <span className="font-medium text-xs text-gray-900">
-                    📢 {item.title}
-                  </span>
+                  <p>
+                    <span className="font-medium lg:text-sm text-xs text-gray-900">
+                      📢 {item.title}
+                    </span>
+
+                    <span className="hidden sm:inline"> {" | "} </span>
+
+                    <span className="block sm:inline text-xs lg:text-sm text-gray-500">
+                      {item.date}
+                    </span>
+                  </p>
 
                   <div className="flex items-center gap-3">
                     <a
@@ -186,9 +198,6 @@ const NoticesSection = () => {
                       <FaFilePdf className="text-red-600" />
                       Download
                     </a>
-                    <span className="text-xs lg:text-sm text-gray-500">
-                      {item.date}
-                    </span>
                   </div>
                 </div>
               ))}
