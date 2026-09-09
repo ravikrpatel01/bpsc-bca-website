094ca9f375a1d897f977e54b112d1732abcbdfe3
Ravi Kumar Patel <ravikumarpatel2001@gmail.com>
2026-09-09 21:20:29 +0530

    updated hero-home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 
 const Hero = () => {
   const images = [
+    "/college_img2.png",
     "/teachers_day_3-modified.jpeg",
     "/teachers_day_1.jpeg",
     "/teachers_day_4.jpeg",
@@ -56,7 +57,6 @@ const Hero = () => {
 
   return (
     <div className="lg:h-[85vh] h-[50vh] relative overflow-hidden">
-
       {/* Background Slider */}
       <div
         className={`absolute inset-0 flex ${
@@ -85,19 +85,14 @@ const Hero = () => {
       {/* Content */}
       <div className="relative z-10 w-full h-full flex items-center px-4 lg:px-10">
         <div className="text-white max-w-2xl">
-
           <h1 className="lg:text-7xl text-4xl font-bold text-yellow-400 leading-tight">
             <span className="text-white">Baijnath Prasad</span> Snehi College
           </h1>
 
           <p className="mt-4 text-xl lg:text-2xl">
             Inspiring the next generation of IT professionals. <br />
-
-            <span className="font-bold text-yellow-300">
-              AICTE Approved
-            </span>
-            , Affiliated to{" "}
-
+            <span className="font-bold text-yellow-300">AICTE Approved</span>,
+            Affiliated to{" "}
             <span className="font-bold text-yellow-300">
               Vinoba Bhave University
             </span>
@@ -105,7 +100,6 @@ const Hero = () => {
           </p>
 
           <div className="lg:mt-10 mt-6 flex gap-2 lg:gap-5">
-
             <Link
               to="/about-college"
               className="bg-emerald-700 hover:bg-emerald-600 flex items-center gap-1 px-2 lg:px-3 py-1 lg:py-2 cursor-pointer text-sm font-medium rounded"
@@ -121,9 +115,7 @@ const Hero = () => {
               <FaPhoneAlt />
               Contact Us
             </Link>
-
           </div>
-
         </div>
       </div>
     </div>
